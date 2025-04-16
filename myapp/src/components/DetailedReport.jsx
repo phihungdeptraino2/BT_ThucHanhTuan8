38e5f1f8d994fb339298209abf51a1ccbcdd3025
phihungdeptraino2 <hungdz1372004@gmail.com>
2025-04-16 16:52:40 +0700

    Cau1. GridCss

---
+++ myapp/src/components/DetailedReport.jsx
@@ -0,0 +1,11 @@
+import avatar1 from "../assets/avatar1.png";
+import { Pencil, Plus } from "lucide-react";
+
+import { useEffect, useState } from "react";
+import "../styles/DetailedReportStyle.css";
+
+const DetailedReport = () => {
+  return <div></div>;
+};
+
+export default DetailedReport;
